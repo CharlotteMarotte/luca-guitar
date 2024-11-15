8bc59232d15c7227e074b8f6ad5c6f1ad29a6370
CharlotteMarotte <charlotte.bilsing@posteo.de>
2024-11-15 20:22:50 +0100

    Implemented zoom-in effect on heading and paragraph

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -40,36 +40,41 @@ const Pricing = () => {
         <section id="pricing" className="relative text-white pt-6">
           <div
             ref={pricingRef}
-            className={`text-center mx-auto max-w-screen-md mb-8 lg:mb-12 transition-opacity duration-1000 ease-in ${isPricingVisible ? "opacity-100" : "opacity-0"}`}
+            className={`transition-opacity duration-1000 ease-in ${isPricingVisible ? "opacity-100" : "opacity-0"}`}
           >
-            <p
-              ref={paragraphRef}
-              className={`mb-5 font-body text-white sm:text-xl transition-opacity duration-1000 ease-in ${isParagraphVisible ? "opacity-100" : "opacity-0"}`}
+            <div
+              ref={pricingRef}
+              className={"text-center mx-auto max-w-screen-md mb-8 lg:mb-12"}
             >
-              Der Vertrag kann{" "}
-              <a
-                href={Contract}
-                target="_blank"
-                rel="noreferrer"
-                className="underline"
+              <p
+                ref={paragraphRef}
+                className={`mb-5 font-body text-white sm:text-xl transition-opacity duration-1000 ease-in ${isParagraphVisible ? "opacity-100" : "opacity-0"}`}
               >
-                hier
-              </a>{" "}
-              eingesehen werden.
-            </p>
+                Der Vertrag kann{" "}
+                <a
+                  href={Contract}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="underline"
+                >
+                  hier
+                </a>{" "}
+                eingesehen werden.
+              </p>
 
-            <ToggleButton
-              isChecked={isWithContract}
-              onToggle={handleToggleChange}
-              activeLabel="mit Vertrag"
-              inactiveLabel="ohne Vertrag"
-            />
-          </div>
+              <ToggleButton
+                isChecked={isWithContract}
+                onToggle={handleToggleChange}
+                activeLabel="mit Vertrag"
+                inactiveLabel="ohne Vertrag"
+              />
+            </div>
 
-          <div className="flex flex-wrap justify-center mt-10 gap-4 sm:gap-6 ">
-            {isWithContract
-              ? pricingPlans.withContract.map(renderPricingCard)
-              : pricingPlans.withoutContract.map(renderPricingCard)}
+            <div className="flex flex-wrap justify-center mt-10 gap-4 sm:gap-6 ">
+              {isWithContract
+                ? pricingPlans.withContract.map(renderPricingCard)
+                : pricingPlans.withoutContract.map(renderPricingCard)}
+            </div>
           </div>
         </section>
       </PageScaffold>
