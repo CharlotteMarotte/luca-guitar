3fa2dcee96235ac3f058894ce59b4f96bccfea6f
CharlotteMarotte <charlotte.bilsing@posteo.de>
2024-11-15 14:40:50 +0100

    added transition to pricing cards on hover

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -34,8 +34,8 @@ const PricingCard = ({ pricingPlan }) => {
       <div
         className={
           pricingPlan.isHighlighted
-            ? 'flex flex-col p-6 mx-auto text-center text-sambuca bg-sand rounded-3xl border-4 border-white shadow h-auto sm:transform sm:translate-y-[-15px] sm:z-10'
-            : 'flex flex-col p-6 mx-auto text-center text-sambuca bg-sand rounded-3xl border-4 border-spicyMustard shadow h-auto'
+            ? 'flex flex-col p-6 mx-auto text-center text-sambuca bg-sand rounded-3xl border-4 border-white shadow h-auto sm:transform sm:translate-y-[-15px] sm:z-10 transition-transform duration-300 ease-in-out md:hover:translate-y-[-3rem]' // Highlighted cards will move more
+            : 'flex flex-col p-6 mx-auto text-center text-sambuca bg-sand rounded-3xl border-4 border-spicyMustard shadow h-auto transition-transform duration-300 ease-in-out md:hover:translate-y-[-3rem]' // Non-highlighted cards will move less
         }
       >
         <h3 className='text-3xl font-heading text-sambuca mb-4'>{pricingPlan.heading}</h3>
