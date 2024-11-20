6f40a45bb745c1664903dfbd082e6317f0603607
CharlotteMarotte <charlotte.bilsing@posteo.de>
2024-11-20 22:03:12 +0100

    WIP: implementing granular cookie options

--- src/components/CookieConsentBanner.jsx
+++ src/components/CookieConsentBanner.jsx
@@ -1,18 +1,58 @@
-import { useCookieConsent } from '@context'
-import CookieConsent from 'react-cookie-consent'
+import React, { useState } from 'react'
+import CookieConsent, { Cookies } from 'react-cookie-consent'
+import { PrimaryButton } from '@components'
 
 const CookieConsentBanner = () => {
-  const { bannerVisible, handleAccept, handleDecline } = useCookieConsent()
+  const [cookieCategories, setCookieCategories] = useState({
+    marketing: false,
+    analytics: false,
+    necessary: true
+  })
+
+  const handleAcceptAll = () => {
+    Cookies.set('analyticsCookie', 'true', { expires: 365 })
+    Cookies.set('marketingCookie', 'true', { expires: 365 })
+    Cookies.set('userConsent', 'true', { expires: 365 })
+  }
+
+  const handleSavePreferences = () => {
+    if (cookieCategories.analytics) {
+      Cookies.set('analyticsCookie', 'true', { expires: 365 })
+    } else {
+      Cookies.remove('analyticsCookie')
+    }
+
+    if (cookieCategories.marketing) {
+      Cookies.set('marketingCookie', 'true', { expires: 365 })
+    } else {
+      Cookies.remove('marketingCookie')
+    }
+
+    Cookies.set('userConsent', 'true', { expires: 365 })
+  }
+
+  const handleDecline = () => {
+    Cookies.remove('analyticsCookie')
+    Cookies.remove('marketingCookie')
+    Cookies.set('userConsent', 'false', { expires: 365 })
+  }
+
+  const handleCategoryChange = (category, isAccepted) => {
+    setCookieCategories((prevState) => ({
+      ...prevState,
+      [category]: isAccepted
+    }))
+  }
 
   return (
     <CookieConsent
-      visible={bannerVisible}
       location='bottom'
-      buttonText='Akzeptieren'
+      buttonText='Alle Akzeptieren'
       declineButtonText='Ablehnen'
       cookieName='userConsent'
-      onAccept={handleAccept}
+      onAccept={handleAcceptAll}
       onDecline={handleDecline}
+      enableDeclineButton
       style={{
         background: '#333',
         zIndex: 9999,
@@ -34,10 +74,29 @@ const CookieConsentBanner = () => {
         borderRadius: '2rem',
         padding: '12px 24px'
       }}
-      enableDeclineButton
     >
-      Diese Website verwendet Cookies, um Ihre Erfahrung zu verbessern. Indem Sie diese Website weiterhin nutzen, stimmen Sie der Verwendung
-      von Cookies zu. Sie können entscheiden, welche Cookie-Kategorien Sie akzeptieren möchten.
+      <div>Auf dieser Seite werden Cookies verwendet, um Inhalte Drittanbieter anzuzeigen. Wähle deine Präferenzen:</div>
+
+      <label>
+        <input type='checkbox' checked={cookieCategories.analytics} onChange={(e) => handleCategoryChange('analytics', e.target.checked)} />
+        Statistik
+      </label>
+
+      <label>
+        <input type='checkbox' checked={cookieCategories.marketing} onChange={(e) => handleCategoryChange('marketing', e.target.checked)} />
+        Marketing
+      </label>
+
+      <label>
+        <input type='checkbox' checked disabled />
+        Essenziell
+      </label>
+
+      <div>
+        <PrimaryButton onClick={handleSavePreferences}>Präferenzen Speichern</PrimaryButton>
+        <PrimaryButton onClick={handleAcceptAll}>Alle Akzeptieren</PrimaryButton>
+      </div>
+
       <a href='/privacy' className='text-copper hover:text-sand'>
         Mehr erfahren
       </a>
